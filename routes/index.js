420222f075eb8bcd647018d0905ee87f32597320
cfibla <charliesuper@hotmail.com>
2016-03-17 22:29:49 +0100

    Quiz+DB 17-3-2016

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,10 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
-router.get('/quizes/question',	quizController.question);
-router.get('/quizes/answer',	quizController.answer);
+
+//Definición de las rutas de /quizes
+router.get('/quizes',							quizController.index);
+router.get('/quizes/:quizId(\\d+)',				quizController.question);
+router.get('/quizes/:quizId(\\d+)/answer',		quizController.answer);
 
 module.exports = router;
