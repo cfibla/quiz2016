b92cf843245e558d8aedecade06407873e501ccc
cfibla <charliesuper@hotmail.com>
2016-05-01 13:51:30 +0200

    sin validaciones 1

--- models/quiz.js
+++ models/quiz.js
@@ -3,14 +3,18 @@
 module.exports = function (sequelize, DataTypes) {
 	return sequelize.define('Quiz',
 	
-	{ pregunta: 	{
-			type: DataTypes.STRING,
-			validate: { notEmpty: {msg: "-> Falta pregunta"}}
-		},
-		respuesta: 	{
-			type: DataTypes.STRING,
-			validate: { notEmpty: {msg: "-> Falta respuesta"}}
-		}
+	{ pregunta: 	
+	//	{
+	//		type: 
+			DataTypes.STRING,
+	//		validate: { notEmpty: {msg: "-> Falta pregunta"}}
+	//	},
+		respuesta: 
+	//		{
+	//		type: 
+			DataTypes.STRING,
+	//		validate: { notEmpty: {msg: "-> Falta respuesta"}}
+	//	}
 	}
 );
 }
